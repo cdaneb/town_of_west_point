868210cedad87a10275fad822441025a2afad48b
Dane Beels <cdanebeels@gmail.com>
2026-04-28 15:07:37 -0400

    static files for Vercel

--- mvp/settings.py
+++ mvp/settings.py
@@ -90,8 +90,8 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 LOGIN_URL = "/login/"
--- mvp/urls.py
+++ mvp/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.shortcuts import redirect
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", lambda request: redirect("login")),
     path("admin/", admin.site.urls),
     path("", include("game.urls")),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
